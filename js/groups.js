db0000c55dad8c55c420ab0c5f3a27863de7b8b7
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2025-12-09 20:23:19 -0500

    Update groups.js

--- js/groups.js
+++ js/groups.js
@@ -68,7 +68,7 @@ async function renderGroupList(container, portalState, options = {}) {
 
   container.innerHTML = `
     <section class="card">
-//      <h2>Groups for ${escapeHtml(portalState.display_name || portalState.project)}</h2>
+      <h2>Groups for ${escapeHtml(portalState.display_name || portalState.project)}</h2>
       <div id="groupsFilters" style="margin-bottom:12px; display:flex; align-items:center; gap:12px; flex-wrap:wrap;">
         <label>Name: <input type="text" id="filter-group-name" value="${escapeHtml(prevName)}" /></label>
         <button id="btnApplyGroupsFilter" class="btn-secondary">Apply Filter</button>
