b3c82249e5efb98676803af8369809aba2298054
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2025-12-21 12:30:12 -0500

    Update contacts.js

--- js/contacts.js
+++ js/contacts.js
@@ -913,8 +913,8 @@ async function renderContactRelationshipsReferralSummary(container, portalState,
       <thead>
         <tr>
           <th>Direction</th>
-          <th>Referred By</th>
           <th>Referred To</th>
+          <th>Referred By</th>
           <th>Financial</th>
           <th>Created</th>
         </tr>
