3a17a3c76631077d42f0c52655f2c1aba022cc35
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2026-01-03 19:44:34 -0500

    Update tab-add-staging.js

--- js/financials/tab-add-staging.js
+++ js/financials/tab-add-staging.js
@@ -223,6 +223,7 @@ export async function loadStagingData() {
 
   renderStagingGrid(rows);
 }
+window.loadStagingData = loadStagingData;
 
 
 /* =========================================================
