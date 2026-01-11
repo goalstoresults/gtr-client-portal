a635ede144b686365347d5c3f6d083cb9104bb0c
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2026-01-11 13:22:48 -0500

    Create tab-campaigns.js

---
+++ js/ecampaigns/tab-campaigns.js
@@ -0,0 +1,10 @@
+export async function renderECCampaigns(container, portalState) {
+  container.innerHTML = `
+    <section class="card">
+      <h3>Campaigns</h3>
+      <p>Loading...</p>
+    </section>
+  `;
+
+  // TODO: fetch campaign list + expanders
+}
