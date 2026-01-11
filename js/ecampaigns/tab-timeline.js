45ea8053d7eda67b66f3717c135dabbc8c5fb5de
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2026-01-11 13:27:21 -0500

    Create tab-timeline.js

---
+++ js/ecampaigns/tab-timeline.js
@@ -0,0 +1,10 @@
+export async function renderECTimeline(container, portalState) {
+  container.innerHTML = `
+    <section class="card">
+      <h3>Campaigns</h3>
+      <p>Loading...</p>
+    </section>
+  `;
+
+  // TODO: fetch campaign list + expanders
+}
