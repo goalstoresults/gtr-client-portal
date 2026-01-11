43132dac78d04d2605dd405575eef386f64d0ad4
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2026-01-11 13:22:18 -0500

    Create tab-overview.js

---
+++ js/ecampaigns/tab-overview.js
@@ -0,0 +1,10 @@
+export async function renderECOverview(container, portalState) {
+  container.innerHTML = `
+    <section class="card">
+      <h3>E‑Campaigns Overview</h3>
+      <p>Loading...</p>
+    </section>
+  `;
+
+  // TODO: fetch stats, year selector, charts, etc.
+}
