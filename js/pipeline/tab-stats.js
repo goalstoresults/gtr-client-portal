c10f465e3fc4d3dfdf300e7d31efdfb7bf8d456a
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2026-01-18 11:58:52 -0500

    Create tab-stats.js

---
+++ js/pipeline/tab-stats.js
@@ -0,0 +1,10 @@
+// js/pipeline/tab-stats.js
+
+export async function renderPipelineStats(container, portalState) {
+  container.innerHTML = `
+    <section class="card">
+      <h2>Pipeline Stats</h2>
+      <p>Analytics coming soon.</p>
+    </section>
+  `;
+}
