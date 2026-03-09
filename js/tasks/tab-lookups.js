8bf959ee3ece15c3259042c2faf05f9032fdb222
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2026-03-09 11:22:27 -0400

    Create tab-lookups.js

---
+++ js/tasks/tab-lookups.js
@@ -0,0 +1,9 @@
+// tab-lookups.js
+export function loadTasksLookups({ portalState, container }) {
+  container.innerHTML = `
+    <div class="card">
+      <h3>Task Lookups</h3>
+      <p>Lookups placeholder (no logic yet)</p>
+    </div>
+  `;
+}
