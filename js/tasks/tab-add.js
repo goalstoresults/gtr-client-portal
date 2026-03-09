ef740bd2b454710fd57ae75b71b24c4f1c9a7f6e
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2026-03-09 11:21:47 -0400

    Create tab-add.js

---
+++ js/tasks/tab-add.js
@@ -0,0 +1,9 @@
+// tab-add.js
+export function loadTasksAdd({ portalState, container }) {
+  container.innerHTML = `
+    <div class="card">
+      <h3>Add Task</h3>
+      <p>Add Task placeholder (no logic yet)</p>
+    </div>
+  `;
+}
