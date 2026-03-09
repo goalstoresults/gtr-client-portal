61b976b89853edc5f06e5226cf22eb7e2e7dfc4b
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2026-03-09 11:22:45 -0400

    Create tab-overview.js

---
+++ js/tasks/tab-overview.js
@@ -0,0 +1,9 @@
+// tab-overview.js
+export function loadTasksOverview({ portalState, container }) {
+  container.innerHTML = `
+    <div class="card">
+      <h3>Overview</h3>
+      <p>Overview placeholder (Phase 2 will fill this in)</p>
+    </div>
+  `;
+}
