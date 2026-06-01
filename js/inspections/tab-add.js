4c30dcfa46de7358e050e2c17578e2514f740af4
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2026-06-01 13:43:46 -0400

    Add renderInspectionAdd function for inspection form

---
+++ js/inspections/tab-add.js
@@ -0,0 +1,10 @@
+// inspections/tab-add.js
+
+export async function renderInspectionAdd(container, portalState) {
+    container.innerHTML = `
+        <section class="card">
+            <h3>Inspections – Add</h3>
+            <p>(Add form will go here)</p>
+        </section>
+    `;
+}
