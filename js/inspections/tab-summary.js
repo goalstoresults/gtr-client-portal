b57eb7f34a9100671abef0054e32b12ae3004197
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2026-06-01 13:44:41 -0400

    Add renderInspectionSummary function for summary view

---
+++ js/inspections/tab-summary.js
@@ -0,0 +1,10 @@
+// inspections/tab-summary.js
+
+export async function renderInspectionSummary(container, portalState) {
+    container.innerHTML = `
+        <section class="card">
+            <h3>Inspections – Summary</h3>
+            <p>(Summary view will go here)</p>
+        </section>
+    `;
+}
