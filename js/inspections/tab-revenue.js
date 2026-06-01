f6bf431d9142c8ab0dadbe44c354913e3a249052
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2026-06-01 13:45:40 -0400

    Create tab-revenue.js

---
+++ js/inspections/tab-revenue.js
@@ -0,0 +1,10 @@
+// inspections/tab-revenue.js
+
+export async function renderInspectionRevenue(container, portalState) {
+    container.innerHTML = `
+        <section class="card">
+            <h3>Inspections – Revenue</h3>
+            <p>(Revenue view will go here)</p>
+        </section>
+    `;
+}
