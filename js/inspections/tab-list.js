fd7edcb3e11b5c868c0693cd005052e39e881fed
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2026-06-01 13:44:15 -0400

    Add renderInspectionList function to display inspections

---
+++ js/inspections/tab-list.js
@@ -0,0 +1,10 @@
+// inspections/tab-list.js
+
+export async function renderInspectionList(container, portalState) {
+    container.innerHTML = `
+        <section class="card">
+            <h3>Inspections – List</h3>
+            <p>(List view will go here)</p>
+        </section>
+    `;
+}
