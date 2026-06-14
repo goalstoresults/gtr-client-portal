bbc317d086504cce686083280c82dbc4d0f7bdc3
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2026-06-14 16:02:39 -0400

    Create tab-client.js

---
+++ js/leads/tab-client.js
@@ -0,0 +1,9 @@
+// js/leads/tab-client.js
+export async function renderLeadClient(container, portalState) {
+  container.innerHTML = `
+    <section class="card">
+      <h2>Client</h2>
+      <p>Linked contact information will load here.</p>
+    </section>
+  `;
+}
