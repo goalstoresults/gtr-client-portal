f3f4ccef2793b1be2ff9a93ad293c4d6894fa6b6
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2026-06-14 16:04:48 -0400

    Create tab-schedule.js

---
+++ js/leads/tab-schedule.js
@@ -0,0 +1,9 @@
+// js/leads/tab-schedule.js
+export async function renderLeadSchedule(container, portalState) {
+  container.innerHTML = `
+    <section class="card">
+      <h2>Schedule</h2>
+      <p>GHL calendar + PDF generation will load here.</p>
+    </section>
+  `;
+}
