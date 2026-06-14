4b266b8bcd8d3c65c99694684bce36959b38ee3d
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2026-06-14 16:05:30 -0400

    Create tab-timeline.js

---
+++ js/leads/tab-timeline.js
@@ -0,0 +1,9 @@
+// js/leads/tab-timeline.js
+export async function renderLeadTimeline(container, portalState) {
+  container.innerHTML = `
+    <section class="card">
+      <h2>Timeline</h2>
+      <p>Lead timeline events will load here.</p>
+    </section>
+  `;
+}
