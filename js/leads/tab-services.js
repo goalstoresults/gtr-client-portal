05193360f2fa4650b9d830fee2ef15c58bc0b5af
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2026-06-14 16:03:57 -0400

    Create tab-services.js

---
+++ js/leads/tab-services.js
@@ -0,0 +1,9 @@
+// js/leads/tab-services.js
+export async function renderLeadServices(container, portalState) {
+  container.innerHTML = `
+    <section class="card">
+      <h2>Services</h2>
+      <p>Lead service selections will load here.</p>
+    </section>
+  `;
+}
