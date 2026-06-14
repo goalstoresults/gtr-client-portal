fab2a0614b01c925a27f1fbedac0c920d0794ed7
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2026-06-14 16:02:16 -0400

    Create tab-list.js

---
+++ js/leads/tab-list.js
@@ -0,0 +1,9 @@
+// js/leads/tab-list.js
+export async function renderLeadsList(container, portalState) {
+  container.innerHTML = `
+    <section class="card">
+      <h2>Lead List</h2>
+      <p>Filter bar + Add Lead button + table will go here.</p>
+    </section>
+  `;
+}
