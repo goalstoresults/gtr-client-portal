a32da5ddf766e433997e9d673d88966875adb438
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2026-06-14 16:04:18 -0400

    Create tab-pricing.js

---
+++ js/leads/tab-pricing.js
@@ -0,0 +1,9 @@
+// js/leads/tab-pricing.js
+export async function renderLeadPricing(container, portalState) {
+  container.innerHTML = `
+    <section class="card">
+      <h2>Pricing Chart</h2>
+      <p>Pricing Engine output will load here.</p>
+    </section>
+  `;
+}
