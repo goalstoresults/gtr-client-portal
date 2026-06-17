437778bb77bddf5a87951a8e22a67b2f274d9812
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2026-06-17 14:37:35 -0400

    Change month format from 2-digit to 1-digit

--- js/utilities.js
+++ js/utilities.js
@@ -87,7 +87,7 @@ export function formatDateTime2(value) {
 
   return d.toLocaleString("en-US", {
     timeZone: "America/New_York",
-    month: "2-digit",
+    month: "1-digit",
     day: "2-digit",
     year: "numeric",
     hour: "numeric",
