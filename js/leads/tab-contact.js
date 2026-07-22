349d645bc40edbc0dd380510655df61522548a7f
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2026-07-22 14:39:53 -0400

    Update tab-contact.js

--- js/leads/tab-contact.js
+++ js/leads/tab-contact.js
@@ -50,8 +50,8 @@ function renderAgentPicker({ container, project, label, agent, onChange }) {
 const url = `
   https://client-portal-api.dennis-e64.workers.dev/api/contacts?
   project=${project}&
+  contact_type=Agent&
   or=(
-    contact_type.eq.Agent,
     first_name.ilike.${encoded},
     last_name.ilike.${encoded},
     business_name.ilike.${encoded},
@@ -60,6 +60,7 @@ const url = `
   )
   &select=contact_id,first_name,last_name,email
 `.replace(/\s+/g, "");
+
 
 
     try {
