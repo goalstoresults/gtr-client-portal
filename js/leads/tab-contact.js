9d6bb606e600b148477004a1ddb5baf58f547eca
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2026-07-22 15:02:03 -0400

    Update tab-contact.js

--- js/leads/tab-contact.js
+++ js/leads/tab-contact.js
@@ -71,7 +71,7 @@ const url = `
             <div class="contact-result"
                  data-id="${a.contact_id}"
                  data-json='${JSON.stringify(a)}'>
-              <strong>${a.first_name} ${a.last_name}</strong><br/>
+              <strong>${a.first_name} ${a.last_name} (${a.contact_type || "No type"})</strong>
               <small>${a.email || "No email"}</small>
             </div>
           `
